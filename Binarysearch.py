0da6b149e723e7ec62356c26b7ea1fe14141a49d
angelothind <115405810+angelothind@users.noreply.github.com>
2023-01-11 11:01:50 +0700

    Add files via upload

---
+++ Binarysearch.py
@@ -0,0 +1,11 @@
+# repeat until array is size 0
+# calc mid point of current array
+# If target is at middle
+# otherwise, if target less than middle change end point to be to the left, otherwise if target bigger than mid cahnge start point
+
+snum_list = [0,3,8,15,17,28,38,51,54,55,59]
+start_point = 0
+end_point = len(snum_list) - 1
+num_of_items = range(len(snum_list[start_point,end_point]))
+num_found = False
+Runout = False
